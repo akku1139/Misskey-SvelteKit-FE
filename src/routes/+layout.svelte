7a4e7ea9b59d0ef357a2435290ce8f7086e17de2
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-13 06:40:28 +0900

    test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<style>
+#container {
+  display: grid;
+  grid-template:
+    "side top  panel" auto
+    "side main panel" auto /
+    auto  auto auto ;
+}
+</style>
+
+<div id="container">
+  <div id="top" style="grid-area: top;">top</div>
+  <div id="sidebar" style="grid-area: side;">side</div>
+  <div id="contents" style="grid-area: main;">main</div>
+  <div id="panel" style="grid-area: panel;">panel</div>
+</div>
